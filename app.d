ca6967ca789dd3ea06bbb5b411cea1645f317646
Saurabh Totey <SaurabhTotey@gmail.com>
2017-04-11 16:04:15 -0600

    Added dictionary and made more of class

--- app.d
+++ app.d
@@ -1,10 +1,14 @@
 import std.stdio;
 import std.random;
+import std.string;
+import std.array;
+import std.file;
+import std.conv;
 
 /**
  * A list of all the possible generatable words
  */
-static string[] vocab = ["apple", "bacon", "doughnut", "smart", "intelligent", "excellent", "school", "clock", "table"];
+static string[] vocab;
 
 /**
  * An unecessary class (I am just using this to practice OO) to handle game stats
@@ -19,10 +23,21 @@ public class HangManBoard {
 	 * <br><strong>amtWrong</strong>: how many times the user has guessed a wrong letter
 	 * <br><strong>amtRight</strong>: how many times the user has guessed a right letter
 	 */
-	public string word;
-	public int points;
-	public int amtWrong;
-	public int amtRight;
+	private string word;
+	private string[] guessed;
+	private int points;
+	private int amtWrong;
+	private int amtRight;
+	private string[] board = [
+		"      _______  ",
+		"     |/      | ",
+		"     |         ",
+		"     |         ",
+		"     |         ",
+		"     |         ",
+		"     |         ",
+		" ____|___      "
+		];
 	
 	/**
 	 * Constructor for a HangManBoard object
@@ -38,15 +53,59 @@ public class HangManBoard {
 	public void getNewWord(){
 		this.word  = vocab[uniform(0, vocab.length)];
 	}
+	
+	/**
+	 * Returns a string representation of the board
+	 */
+	override public string toString(){
+		string boardAsString = format(
+			board[0] ~ "\n" ~
+			board[1] ~ "\tYou have %s points\n" ~
+			board[2] ~ "\tYour correct/incorrect percentage is %s\n" ~
+			board[3] ~ "\n" ~
+			board[4] ~ "\n" ~
+			board[5] ~ "\n" ~
+			board[6] ~ "\n" ~
+			board[7] ~ "\tSo far, you have guessed the letters", this.points, (this.amtRight + this.amtWrong == 0)?  "?" : to!string(100 * this.amtRight / (this.amtRight + this.amtWrong)) ~ "%");
+		foreach(string individualGuess ; this.guessed){
+			boardAsString ~= individualGuess ~ ", ";
+		}
+		boardAsString ~= "\b\b";
+		return boardAsString;
+	}
+	
+	/**
+	 * Adds one more to the amtWrong variable and changes the board accordingly
+	 */
+	public void incrementWrong(){
+		amtWrong++;
+		if(amtWrong > 6){
+			this.board[5] = "     |      / \\";
+		}else if(amtWrong > 5){
+			this.board[5] = "     |      /  ";
+		}else if(amtWrong > 4){
+			this.board[4]  ="     |       | ";
+		}else if(amtWrong > 3){
+			this.board[3] = "     |      \\|/";
+		}else if(amtWrong > 2){
+			this.board[3] = "     |      \\| ";
+		}else if(amtWrong > 1){
+			this.board[3] = "     |       | ";
+		}else if(amtWrong > 0){
+			this.board[2] = "     |      (_)";
+		}
+	}
 }
 
 /**
  * Main entry point and beginning of program
  */
 static void main() {
-	//TODO read dictionary file and add words to vocab
+	File file = File("E:\\Documents\\Eclipse D Workspace\\Hangman\\src\\listOfWords.txt", "r");
+	vocab = file.byLineCopy().array();
+	file.close();
 	HangManBoard game = new HangManBoard();
-	writeln(game.word);
+	writeln(game);
 }
 
 /**
