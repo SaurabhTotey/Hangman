dee5e879164c859da1ec77c1e8a99ddf1c200e04
Saurabh Totey <SaurabhTotey@gmail.com>
2017-04-12 08:46:47 -0600

    Made basic working game and deleted unneeded class

--- app.d
+++ app.d
@@ -4,98 +4,30 @@ import std.string;
 import std.array;
 import std.file;
 import std.conv;
+import std.regex;
+import std.algorithm.searching;
 
 /**
- * A list of all the possible generatable words
+ * A list of all of the needed data throughout the game
  */
 static string[] vocab;
-
-/**
- * An unecessary class (I am just using this to practice OO) to handle game stats
- * All of this could be done statically, as this is only ever instantiated once
- */
-public class HangManBoard {
-	
-	/**
-	 * Instance data for a HangManBoard class
-	 * <br><strong>word</strong>: the word that the user is currently trying to guess
-	 * <br><strong>points</strong>: the amount of points the user has acculmulated from correct guesses
-	 * <br><strong>amtWrong</strong>: how many times the user has guessed a wrong letter
-	 * <br><strong>amtRight</strong>: how many times the user has guessed a right letter
-	 */
-	private string word;
-	private string[] guessed;
-	private int points;
-	private int amtWrong;
-	private int amtRight;
-	private string[] board = [
-		"      _______  ",
-		"     |/      | ",
-		"     |         ",
-		"     |         ",
-		"     |         ",
-		"     |         ",
-		"     |         ",
-		" ____|___      "
-		];
-	
-	/**
-	 * Constructor for a HangManBoard object
-	 * Automatically gives itself a word
-	 */
-	public this(){
-		this.getNewWord();
-	}
-	
-	/**
-	 * Generates a random new word and sets the current word as the new word
-	 */
-	public void getNewWord(){
-		this.word  = vocab[uniform(0, vocab.length)];
-	}
-	
-	/**
-	 * Returns a string representation of the board
-	 */
-	override public string toString(){
-		string boardAsString = format(
-			board[0] ~ "\n" ~
-			board[1] ~ "\tYou have %s points\n" ~
-			board[2] ~ "\tYour correct/incorrect percentage is %s\n" ~
-			board[3] ~ "\n" ~
-			board[4] ~ "\n" ~
-			board[5] ~ "\n" ~
-			board[6] ~ "\n" ~
-			board[7] ~ "\tSo far, you have guessed the letters", this.points, (this.amtRight + this.amtWrong == 0)?  "?" : to!string(100 * this.amtRight / (this.amtRight + this.amtWrong)) ~ "%");
-		foreach(string individualGuess ; this.guessed){
-			boardAsString ~= individualGuess ~ ", ";
-		}
-		boardAsString ~= "\b\b";
-		return boardAsString;
-	}
-	
-	/**
-	 * Adds one more to the amtWrong variable and changes the board accordingly
-	 */
-	public void incrementWrong(){
-		amtWrong++;
-		if(amtWrong > 6){
-			this.board[5] = "     |      / \\";
-		}else if(amtWrong > 5){
-			this.board[5] = "     |      /  ";
-		}else if(amtWrong > 4){
-			this.board[4]  ="     |       | ";
-		}else if(amtWrong > 3){
-			this.board[3] = "     |      \\|/";
-		}else if(amtWrong > 2){
-			this.board[3] = "     |      \\| ";
-		}else if(amtWrong > 1){
-			this.board[3] = "     |       | ";
-		}else if(amtWrong > 0){
-			this.board[2] = "     |      (_)";
-		}
-	}
-}
+static string word;
+static string[] displayableWord;
+static string[] guessed;
+static int points;
+static int amtWrong;
+static int amtRight;
+static string[] board = [
+	"      _______  ",
+	"     |/      | ",
+	"     |         ",
+	"     |         ",
+	"     |         ",
+	"     |         ",
+	"     |         ",
+	" ____|___      "
+	];
+immutable static int wrongToEndGame = 7;	
 
 /**
  * Main entry point and beginning of program
@@ -104,8 +36,18 @@ static void main() {
 	File file = File("E:\\Documents\\Eclipse D Workspace\\Hangman\\src\\listOfWords.txt", "r");
 	vocab = file.byLineCopy().array();
 	file.close();
-	HangManBoard game = new HangManBoard();
-	writeln(game);
+	getNewWord();
+	writeln(gameString());
+	while(amtWrong < wrongToEndGame){
+		stdout.flush();
+		string input = strip(stdin.readln());
+		do{
+			input = strip(stdin.readln());
+		}while(input == "");
+		clearLog();
+		interpretInput(input);
+		writeln(gameString());
+	}
 }
 
 /**
@@ -114,5 +56,87 @@ static void main() {
 static void clearLog() {
 	for(int i = 0; i < 100; i++){
 		writeln();
+	}
+}
+
+/**
+ * Generates a random new word and sets the current word as the new word
+ */
+static void getNewWord(){
+	word = vocab[uniform(0, vocab.length)];
+	displayableWord = null;
+	for(int i = 0; i < word.length; i++){
+		displayableWord ~= "_";
+	}
+}
+
+/**
+ * Returns a string representation of the board
+ */
+static string gameString(){
+	string boardAsString = format(
+		board[0] ~ "\n" ~
+		board[1] ~ "\tYou have %s points\n" ~
+		board[2] ~ "\tYour correct/incorrect percentage is %s\n" ~
+		board[3] ~ "\n" ~
+		board[4] ~ "\n" ~
+		board[5] ~ "\n" ~
+		board[6] ~ "\n" ~
+		board[7] ~ "\tSo far, you have guessed the letters ", points, (amtRight + amtWrong == 0)?  "?" : to!string(100 * amtRight / (amtRight + amtWrong)) ~ "%");
+	foreach(string individualGuess ; guessed){
+		boardAsString ~= (individualGuess == guessed[guessed.length - 1])? individualGuess : individualGuess ~ ", ";
+	}
+	boardAsString ~= "\n\n";
+	foreach(string wordProgressChar ; displayableWord){
+		boardAsString ~= " " ~ wordProgressChar;
+	}
+	return boardAsString;
+}
+
+/**
+ * Adds one more to the amtWrong variable and changes the board accordingly
+ */
+static void incrementWrong(){
+	amtWrong++;
+	points -= 2;
+	if(amtWrong > 6){
+		board[5] = "     |      / \\";
+	}else if(amtWrong > 5){
+		board[5] = "     |      /  ";
+	}else if(amtWrong > 4){
+		board[4]  ="     |       | ";
+	}else if(amtWrong > 3){
+		board[3] = "     |      \\|/";
+	}else if(amtWrong > 2){
+		board[3] = "     |      \\| ";
+	}else if(amtWrong > 1){
+		board[3] = "     |       | ";
+	}else if(amtWrong > 0){
+		board[2] = "     |      (_)";
+	}
+}
+
+/**
+ * Interprets whatever is passed to it and game is updated accordingly
+ */
+static void interpretInput(string toInterpret){
+	guessed ~= toInterpret;
+	string[] splitString = split(word, toInterpret);
+	int amtInString = splitString.length - 1;
+	if(amtInString == 0){
+		incrementWrong();
+		return;
+	}
+	int ptsToAdd;
+	for(int i = 0; i < toInterpret.length; i++){
+		char character = toInterpret[i];
+		ptsToAdd += (canFind("aeiou", character))? 1 : 3;
+		amtRight++;
+	}
+	points += amtInString * ptsToAdd;
+	foreach(string stillExists ; splitString){
+		for(int i = 0; i < toInterpret.length; i++){
+			displayableWord[stillExists.length + i] = to!string(toInterpret[i]);
+		}
 	}
 }
